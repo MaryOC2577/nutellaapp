5bebbf2b92f5a0deea504b2506870c842b6efa86
Alanthea <marybot@free.fr>
2022-04-01 15:46:22 +0200

    add urls to search app

---
+++ search/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+
+urlpatterns = [
+    path("", views.SearchView.as_view(), name="search"),
+    path("result/", views.ProductResult.as_view(), name="result"),
+    path("oneproduct/<int:pk>/", views.OneProduct.as_view(), name="oneproduct"),
+    path("substitutes/<int:pk>/", views.Substitutes.as_view(), name="substitutes"),
+    path(
+        "savefavorite/<int:pk>/",
+        views.SaveFavorites.as_view(),
+        name="favorite",
+    ),
+]
