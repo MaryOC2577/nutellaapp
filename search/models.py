75987f3f53ea05b4144d909348118851e1ee5624
Alanthea <marybot@free.fr>
2022-04-01 15:45:08 +0200

    add models in search app

--- search/models.py
+++ search/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.views.generic import ListView
+from nutella.models import Product, Category
 
-# Create your models here.
+
+class SearchProduct(ListView):
+    model = Product
+    context_object_name = "products"
