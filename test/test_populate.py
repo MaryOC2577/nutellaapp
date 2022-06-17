e448c0db874025c82987ce205e4c48c7a35db669
Alanthea <marybot@free.fr>
2022-06-17 17:39:42 +0200

    update tests

--- test/test_populate.py
+++ test/test_populate.py
@@ -2,6 +2,16 @@ from nutella.management.commands import populate_db
 
 class TestPopulate():
 
+    def get_populate(self):
+        return populate_db
 
     def test_populate():
-        
+
+        def monkey_populate():
+            cmd_populate = populate_db
+            return cmd_populate
+
+        monkeypatch.setattr(cmd_populate, "pizza", monkey_populate())
+
+        cmd = get_populate()
+        assert cmd == cmd_populate
