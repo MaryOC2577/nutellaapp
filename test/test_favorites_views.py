38c7ee79fe7d1096c9ea31fa399352883decabec
Alanthea <marybot@free.fr>
2022-07-06 21:06:35 +0200

    update tests

--- test/test_favorites_views.py
+++ test/test_favorites_views.py
@@ -12,5 +12,15 @@ class TestFavoriteViews(TestCase):
  
         # Setup request and view.
         response = self.client.get('/favorites/')
+        print("url : ", response)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'favorite_list.html')
+
+      
+    def test_favorites_set_in_context(self):
+        request = RequestFactory().get('favorites')
+        view = ShowFavorites()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn('favorites', context)
