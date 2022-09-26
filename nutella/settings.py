617f6965143054b8051c8e0ed9d52b14c82aac79
MaryOC2577 <62305308+MaryOC2577@users.noreply.github.com>
2022-09-26 14:18:16 +0200

    deploy on scaleway

--- nutella/settings.py
+++ nutella/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'nutella.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "nutella",
-#         "USER": "nutuser",
-#         "PASSWORD": "nut77ella25",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.postgresql",
+         "NAME": "rdb-zealous-pascal",
+         "USER": "nutuser",
+         "PASSWORD": "Nut77ella@",
+         "HOST": "51.158.103.28",
+         "PORT": "45754",
+     }
+}
 
 # developpement
-DATABASES = {'default': dj_database_url.config(os.getenv('DATABASE_URL'))}
+#DATABASES = {'default': dj_database_url.config(os.getenv('DATABASE_URL'))}
 
 
 AUTH_PASSWORD_VALIDATORS = [
