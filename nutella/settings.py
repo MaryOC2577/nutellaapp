8ea1e28cece39cbeecb1e25eafb22cb1dce3a90c
MaryOC2577 <62305308+MaryOC2577@users.noreply.github.com>
2022-10-05 20:44:51 +0200

    Update settings.py

--- nutella/settings.py
+++ nutella/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
